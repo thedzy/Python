0b129ca0eaa8d23565793ae138ee33593179ce4b
thedzy <thedzy@hotmail.com>
2018-10-11 22:00:25 +0200

    Added mysql_connection.py

--- Snippits/README.md
+++ Snippits/README.md
@@ -1,3 +1,5 @@
 # Snippits
 Examples of functionality that can be embedded or used in other scripts
 
+## mysql_connection.py
+Simple mySQL database connecion and query
---
+++ Snippits/mysql_connection.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import mysql.connector #pip3 install mysql-connector==2.1.4
+from mysql.connector import (connection)
+from mysql.connector import errorcode
+
+# DB connection inforation
+hostname = 'db.example.com'
+username = 'databaseuser'
+password = 'databasepass'
+database = 'example'
+
+print ("Using mysql.connector")
+
+try:
+	# Initilaise database
+	mysqlconn = mysql.connector.connect( host=hostname, user=username, passwd=password, database=database )
+except mysql.connector.Error as err:
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("Something is wrong with your user name or password")
+	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("Database does not exist")
+	else:
+		print("Connection error %s" % err)
+else:
+	print("Connected")
+	# Connect
+	cursor = mysqlconn.cursor()
+
+	# Query
+	query = ("SELECT COUNT(*) from table1")
+	cursor.execute(query)
+
+	count=cursor.fetchone()[0]
+
+	# Close connections
+	cursor.close()
+	mysqlconn.close()
+
+	print (count)
