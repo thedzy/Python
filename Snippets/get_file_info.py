0eec27d3fbd6c6da697cd75f6144b0064be42ef1
thedzy <thedzy@hotmail.com>
2018-11-22 19:36:27 +0100

    Added get_file_info.py

---
+++ Snippets/get_file_info.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os
+import time
+
+file_stats = os.stat("get_file_info.py")
+
+print("mode  = {0}".format(oct(file_stats[0])[-4:]))
+print("ino   = {0}".format(file_stats[1]))
+print("dev   = {0}".format(file_stats[2]))
+print("nlink = {0} links".format(file_stats[3]))
+print("uid   = {0}".format(file_stats[4]))
+print("gid   = {0}".format(file_stats[5]))
+print("size  = {0:0.2f}kb".format(file_stats[6]/1024))
+print("atime = {0}".format(time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(file_stats[7]))))
+print("mtime = {0}".format(time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(file_stats[8]))))
+print("ctime = {0}".format(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(file_stats[9]))))
+
