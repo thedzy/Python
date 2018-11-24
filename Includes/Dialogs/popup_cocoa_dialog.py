794dbcbc371b94039ed8e469fc969d38aaec622a
thedzy <thedzy@hotmail.com>
2018-11-24 14:35:24 +0100

    Added popup_cocoa_dialog_example.py and an example

---
+++ Includes/Dialogs/popup_cocoa_dialog.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+"""
+Script:	popup_input.py
+Date:	2018-11-24
+
+Platform: MacOS
+
+Description:
+Creates a GUI input dialog using the PyObjC framework
+
+"""
+__author__      = "thedzy"
+__copyright__   = "Copyright 2018, thedzy"
+__license__     = "GPL"
+__version__     = "1.0"
+__maintainer__  = "thedzy"
+__email__       = "thedzy@hotmail.com"
+__status__      = "Developer"
+
+try:
+	from AppKit import *
+except:
+	print("You need to have the PyObjC frameworks installed")
+	print("easy_install -U pyobjc")
+
+class Alert(object):
+	"""
+	Alert (Class)
+	Create a message dialog and get user response
+	"""
+
+	WARNING = NSWarningAlertStyle
+	INFORMATIONAL = NSInformationalAlertStyle
+	CRITICAL = NSCriticalAlertStyle
+
+	def __init__(self, messageText, **kwargs):
+		"""
+		__init__
+		Initialise dialog
+		:param messageText: "The message for the dialog
+		:param kwargs:
+			informativeText = Information below the alert message (string)
+			icon = Path to icon (string)
+			buttons = Button texts (list, strings)
+			style = WARNING, INFORMATIONAL or CRITICAL
+		"""
+		super(Alert, self).__init__()
+		self.messageText = messageText
+		self.informativeText = kwargs.get('informativeText',"")
+		self.icon = kwargs.get('icon', None)
+		self.buttons = kwargs.get('buttons',['OK'])
+		if not kwargs.get('style', self.INFORMATIONAL) in [ self.INFORMATIONAL, self.INFORMATIONAL, self.CRITICAL ]:
+			self.style = self.INFORMATIONAL
+		else:
+			self.style = kwargs.get('style', self.INFORMATIONAL)
+
+	def display(self):
+		"""
+		Display the message and return the result
+		:return: Button Index (integer)
+		"""
+		alert = NSAlert.alloc().init()
+		alert.setMessageText_(self.messageText)
+		alert.setInformativeText_(self.informativeText)
+		try:
+			if not self.icon is None:
+				image = NSImage.alloc().initByReferencingFile_(self.icon)
+				alert.setIcon_(image)
+		except:
+			pass
+		alert.setAlertStyle_(self.style)
+		for button in reversed(self.buttons):
+			alert.addButtonWithTitle_(button)
+		NSApp.activateIgnoringOtherApps_(True)
+		self.buttonPressed = alert.runModal()
+		return len(self.buttons) - (self.buttonPressed - 1000) - 1
+
---
+++ Includes/Dialogs/popup_cocoa_dialog_example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from popup_cocoa_dialog import *
+
+def main():
+	buttons = ['Cancel', 'Yes', 'No']
+	alert_msg = Alert("Alert Message", informativeText='Information about the alert and what do or choose', icon='/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertStopIcon.icns', buttons=buttons, style=Alert.INFORMATIONAL)
+	print("You pressed: " + buttons[alert_msg.display()])
+
+if __name__ == "__main__":
+	main()
