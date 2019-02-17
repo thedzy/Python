613d6b6e0aa923709e6332956e73cd724c540976
thedzy <thedzy@hotmail.com>
2019-02-17 19:36:12 +0100

    Corrected tabbing

--- Snippets/run_as_another_user_bash.py
+++ Snippets/run_as_another_user_bash.py
@@ -17,6 +17,7 @@ def main():
 	# If you want to seperate the multiline input into an array
 	print(run_result[0].decode("utf-8").splitlines())
 
+
 def run_bash_as_user(cmd, user_name):
 	"""
 	Runs a command (SH, BASH) as the user passed
@@ -24,28 +25,31 @@ def run_bash_as_user(cmd, user_name):
 	:param user_name: (String) Username, if user_name is invlaid, it will run as the current user
 	:return: (Tupple) stdout and estderr
 	"""
+
 	def run_as_user(user_name):
-	    """
-	    Passes a function to set the user, can be used in Popen
-	    :param user_name: (String) Username
-	    :return: Function or null if err
-	    """
-	    try:
-	        pwnam = pwd.getpwnam(user_name)
-	        user_uid = pwnam.pw_uid
-	        user_gid = pwnam.pw_gid
+		"""
+		Passes a function to set the user, can be used in Popen
+		:param user_name: (String) Username
+		:return: Function or null if err
+		"""
+		try:
+			pwnam = pwd.getpwnam(user_name)
+			user_uid = pwnam.pw_uid
+			user_gid = pwnam.pw_gid
 
-	        def preexec_fn():
-	            os.setgid(user_gid)
-	            os.setuid(user_uid)
-	        return preexec_fn
-	    except:
-	        return None
+			def preexec_fn():
+				os.setgid(user_gid)
+				os.setuid(user_uid)
 
-	cmd_result = subprocess.Popen(cmd, preexec_fn=run_as_user(user_name), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+			return preexec_fn
+		except:
+			return None
+
+	cmd_result = subprocess.Popen(cmd, preexec_fn=run_as_user(user_name), shell=True, stdout=subprocess.PIPE,
+	                              stderr=subprocess.PIPE).communicate()
 
 	return cmd_result
 
-if __name__ == "__main__":
-    main()
 
+if __name__ == "__main__":
+	main()
