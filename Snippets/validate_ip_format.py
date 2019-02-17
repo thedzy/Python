8f072028f3d442f910947e48f1d8cf38eafd8357
thedzy <thedzy@hotmail.com>
2019-02-17 21:22:30 +0100

    Added validate_ip_format.py

---
+++ Snippets/validate_ip_format.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+import socket
+
+
+ip = sys.argv[1]
+
+try:
+	socket.inet_pton(socket.AF_INET, ip)
+	print("Valid IPv4")
+except socket.error:
+	try:
+		socket.inet_pton(socket.AF_INET6, ip)
+		print("Valid IPv6")
+	except socket.error:
+		print("Invalid")
