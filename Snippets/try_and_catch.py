d082054bf68df9a88b63c52b980be3d813bf2091
thedzy <thedzy@hotmail.com>
2019-02-23 10:39:20 +0100

    Added finally statement

--- Snippets/try_and_catch.py
+++ Snippets/try_and_catch.py
@@ -3,8 +3,15 @@
 try:
 	# Will fail:
 	os.mkdir("/tmp/path/does/not exist")
-	
-	# Will succeed
-	#os.makedirs("/tmp/path/does/not exist")
 except:
 	print("Performing error correction\n")
+finally:
+	print("Do this regardless"
+
+try:
+	# Will succeed
+	os.makedirs("/tmp/path/does/not exist")
+except:
+	print("Performing error correction\n")
+finally:
+	print("Do this regardless"
