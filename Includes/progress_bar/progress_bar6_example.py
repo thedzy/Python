6c8084b1dd8238ba9b07a6f19d44d64dcbe200af
thedzy <thedzy@hotmail.com>
2021-03-13 17:10:13 +0100

    Updated example

--- Includes/progress_bar/progress_bar6_example.py
+++ Includes/progress_bar/progress_bar6_example.py
@@ -1,25 +1,60 @@
 #!/usr/bin/env python3
 
-from progress_bar6 import *
+from progress_bar6 import ProgressBar
+import time
 
 
 def main():
-	progressLength = 100
+    lines = [
+        'Reading install',
+        '.', '.', '.', '.',
+        '\n',
+        'Unpacking',
+        '.', '.', '.', '.',
+        '\n',
+        'Loading compressed files',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+        '\n',
+        'Decompressing',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+        '\n'
+        'Installing',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+        '\n'
+        'Configuring',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+        '\n'
+        'Completing',
+        '.', '.', '.',
+        '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
+    ]
 
-	app = ProgressBar()
-	app.setTitle('Progressing...')
-	app.setMax(progressLength)
-	app.setColour('#00aa00', '#ffffff')
-	app.setDeterminante(True)
+    progress_length = len(lines)
 
-	for progressCount in range(progressLength):
-		time.sleep(.1)  # do real work here
-		if app.isActive():
-			# Incerement by 1
-			app.setIncrement(1)
-			app.setLabel('Loading... {0}'.format(progressCount))
-		app.addOutput(str(progressCount) + '\n -- ')
+    # Initialise with settings
+    app = ProgressBar(title='Loading', set_max=progress_length, foreground=(0.5, 0.0, 0.0))
+
+    # Or set after
+    app.set_title('Progressing...')
+    app.set_max(progress_length)
+    app.set_colour('#00aa00', (255, 255, 255))
+    app.set_determinante(True)
+
+    for progress_count in range(progress_length):
+        time.sleep(.05)  # Do real work here
+        if app.is_active():
+            # Increment by 1
+            app.set_increment(1)
+            app.set_label('Loading... {0}'.format(progress_count))
+
+        # Display messages
+        app.add_output(lines[progress_count])
+
+    app.add_output('Done', reset=True)
+    time.sleep(1.0)
 
 
 if __name__ == '__main__':
-	main()
+    main()
