9fb2aab3660d58c186f184288f2d43fd0a5e6ada
thedzy <thedzy@thedzy.com>
2021-03-13 19:41:27 +0100

    Updated to relect changes in class

--- Includes/progress_bar/progress_bar5_example.py
+++ Includes/progress_bar/progress_bar5_example.py
@@ -2,25 +2,23 @@
 
 from progress_bar5 import *
 import time
+from pathlib import Path
 
 
 def main():
-	progressLength = 100
+    base_path = Path.home().joinpath('Downloads/')
+    files = [file for file in base_path.iterdir() if base_path.is_dir()]
+    progress_length = len(files)
 
-	app = ProgressBar()
-	app.setTitle('Progressing...')
-	app.setMax(progressLength)
-	app.setColour('green', 'white')
-	app.setDeterminante(True)
+    app = ProgressBar(progress_length, title='Loading files', width=1000, bar_colour=(0, 128, 0), back_colour='#ccffcc')
 
-	for progressCount in range(progressLength):
-		time.sleep(0.05)  # do real work here
-		if app.isActive():
-			# Alt: Set the value to progressCount
-			app.setPosition(progressCount)
-			# Incerement by 1
-			app.setIncrement(1)
+    for file in files:
+        app.set_label('Progressing...{}'.format(file.name))
+        time.sleep(5 / len(files))  # do real work here
+        if app.is_active():
+            # Increment by 1
+            app.increment(1)
 
 
 if __name__ == '__main__':
-	main()
+    main()
